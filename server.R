3048794127896395af06128986a30533f673c22c
myeelang <78327219+myeelang@users.noreply.github.com>
2022-04-22 11:24:25 -1000

    Add files via upload

--- server.R
+++ server.R
@@ -8,42 +8,17 @@ library(shiny)
 library(UsingR)
 
 shinyServer(function(input, output) {
-        data("galton")
-        model <- reactive({
-        brushed_data <- brushedPoints(galton, input$brush1,
-                                      xvar = "parent", yvar = "child")
-        if(nrow(brushed_data) < 2){
-            return(NULL)
-        }
-        lm(child ~ parent, data = brushed_data)
-    })
-    output$slopeOut <- renderText({
-        if(is.null(model())){
-            "No Model Found"
-        } else {
-            model()[[1]][2]
-        }
-    })
-    output$intOut <- renderText({
-        if(is.null(model())){
-            "No Model Found"
-        } else {
-            model()[[1]][1]
-        }
-    })
-    output$cheight <- renderText({
-        if(is.null(model())){
-            "No Model Found"
-        } else {
-        model()[[1]][1] + model()[[1]][2]*input$pheight
-        }
-    })
-        output$plot1 <- renderPlot({
+    data("galton")
+    model <- lm(child ~ parent, data = galton)
+    output$plot1 <- renderPlot({
         plot(galton$parent, galton$child, xlab = "Parent (in inches)",
              ylab = "Child (in inches)", main = "Galton's Heights Data of Parents & their Children",
              cex = 1.0, pch = 16, bty = "n")
-        if(!is.null(model())){
-            abline(model(), col = "blue", lwd = 2)
-        }
+        if(input$lmline){
+            abline(model, col = "blue", lwd = 2)
+            }
+            })
+    output$cheight <- renderText({
+        model[[1]][1] + model[[1]][2]*input$pheight
+        })
     })
-})
--- ui.R
+++ ui.R
@@ -7,27 +7,24 @@
 library(shiny)
 
 shinyUI(fluidPage(
-    titlePanel("Modeling and Prediction using Galton's Height Data for Parents and Children"),
+    titlePanel("Predicting Child Height using Parents' Height"),
     sidebarLayout(
         sidebarPanel(
-            h3("Model Results from Step 1:"),
-            h4("Intercept:"),
-            textOutput("intOut"),
-            h4("Slope:"),
-            textOutput("slopeOut"),
-            h2("Step 2: Prediction:"),
+            h3("Slide the bar to select Parent's Height"),
+
             sliderInput("pheight",
-                        "Slide to pick parents' height (in inches):",
+                        "Parents' height (in inches):",
                         min = 1,
                         max = 100,
                         value = 65),
-            h3("Child's height prediction from Step 2:"),
-            textOutput("cheight")        ),
+            h3("Check to display Linear Model line"),           
+            checkboxInput("lmline", "Show/Hide Linear Model line")
+        ),
         mainPanel(
-            h2("Step 1: Drag a box to select data sample for the prediction model:"),
-            plotOutput("plot1", brush = brushOpts(
-                id = "brush1")
+            h3("Predicted Child's height:"),
+            textOutput("cheight"),
+            plotOutput("plot1")
             )
         )
     )
-))
+)
